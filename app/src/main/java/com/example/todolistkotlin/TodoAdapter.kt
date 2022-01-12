22a476f5f5fcd2f7387c56a264116c263522c3d9
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-01-12 13:50:54 +0000

    imported TextView into TodoAdapter

--- app/src/main/java/com/example/todolistkotlin/TodoAdapter.kt
+++ app/src/main/java/com/example/todolistkotlin/TodoAdapter.kt
@@ -4,6 +4,7 @@ import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_todo.view.*
 
